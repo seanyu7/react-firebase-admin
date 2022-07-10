a2568e581b56a90cdd92eab4d85be781b761c530
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-10 18:20:47 +0900

    ⚡️ adding strorage database in my application

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBs7WOgN6JR9NJ2KQtMQl7hcqyHIa0mOFc",
@@ -16,4 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth();
-
+export const storage = getStorage(app);
