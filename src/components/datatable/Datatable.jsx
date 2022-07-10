41c3d5e02843de9e07ce246afbc3d5d1d97fe2bf
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-10 21:13:22 +0900

    ⚡️ fetching all the data from firebase

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -2,10 +2,30 @@ import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../datatablesource";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { db } from "../../firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 const Datatable = () => {
-  const [data, setData] = useState(userRows);
+  let list=[];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "users"));
+        querySnapshot.forEach((doc) => {
+          list.push(doc);
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData()
+  }, []);
+
+console.log(data);
+
 
   const handleDelete = (id) => {
     setData(data.filter((item) => item.id !== id));
